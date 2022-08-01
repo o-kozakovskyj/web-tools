9a5924e7f2e34beacd17023e29e22e53cf4a5d5d
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-08-01 21:28:33 +0300

    fix

--- lesson4/task1/index.js
+++ lesson4/task1/index.js
@@ -7,7 +7,7 @@
 
 
 import calcExpression, {sum, mult} from './calculator/index.js';
-import fetchUser from './profile/gateWay.js';
+import fetchUser from './profile/gateway.js';
 import { printProfile } from './profile/index.js';
 import('./polyfills/array-flatMap.js');
 import('./polyfills/array-flat.js');
