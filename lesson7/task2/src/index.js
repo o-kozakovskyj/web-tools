75033268dfcb5657d68cd4d7df1c0c9fabef1c4b
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-08-04 18:32:44 +0300

    todo

---
+++ lesson7/task2/src/index.js
@@ -0,0 +1,21 @@
+import { initTodoListHandlers } from './list/todoList.js';
+import { renderTasks } from './list/renderer.js';
+import { getTasksList } from './list/tasksGateway.js';
+import { setItem } from './list/storage.js';
+import './index.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+  getTasksList().then((tasksList) => {
+    setItem('tasksList', tasksList);
+    renderTasks();
+  });
+
+  initTodoListHandlers();
+});
+const onStorageChange = (event) => {
+  if (event.key === 'tasksList') {
+    renderTasks();
+  }
+};
+
+window.addEventListener('storage', onStorageChange);
--- lesson7/task2/webpack.config.js
+++ lesson7/task2/webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -9,17 +10,21 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.s?css$/i,
+        test: /.js$/,
+        use: ['babel-loader']
+      },
+      {
+        test: /.s?css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
       },
       {
-        test: /\.(jpg|png|gif)$/i,
+        test: /.(jpg|png)$/,
         use: [
           {
             loader: 'url-loader',
             options: {
               limit: 8192,
-              name:'[name].[ext]',
+              name: '[name].[ext]',
               outputPath: 'images',
             },
           },
@@ -28,11 +33,13 @@ module.exports = {
     ],
   },
   plugins: [
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
     new MiniCssExtractPlugin({
-filename: '[name].css',
+      filename: '[name].css',
     }),
   ],
 };
